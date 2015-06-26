4e9b3971cac493b9bb9f19ea8895fa9e675f1971
ahopper-soltech <andy.hopper@soltech.net>
2015-06-26 15:03:32 -0400

    Fixed ImageResourceExtension - it used to never work as it was always trying to load the resource from the SolTech.Xamarin.Forms resource streams and not the caller. I also added support for (optionally) explicitly naming the assembly from which to load the resource using the <resource>;assembly=<assembly> syntax.

--- Soltech.Xamarin.Forms/MarkupExtensions/ImageResourceExtension.cs
+++ Soltech.Xamarin.Forms/MarkupExtensions/ImageResourceExtension.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Text.RegularExpressions;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -10,15 +13,65 @@ namespace SolTech.Forms
     [ContentProperty("Source")]
     public class ImageResourceExtension : IMarkupExtension
     {
+        private static readonly Regex _resourceSplitter = new Regex("(?<resourceName>[^;]+)(?:;assembly=(?<assemblyName>.+))?");
+
         public string Source { get; set; }
 
         public object ProvideValue(IServiceProvider serviceProvider)
         {
             if (String.IsNullOrEmpty(Source))
+            {
+                System.Diagnostics.Debug.WriteLine("ImageResourceExtension::ProvideValue - No value supplied for Source.");
                 return null;
+            }
+            System.Diagnostics.Debug.WriteLine("ImageResourceExtension::ProvideValue - Attempting to load resource '{0}'...", Source);
 
-            // Do your translation lookup here, using whatever method you require
-            var imageSource = ImageSource.FromResource(Source);
+            ImageSource imageSource = null;
+            var match = _resourceSplitter.Match(Source);
+            if (match.Success)
+            {
+                var resourceName = match.Groups["resourceName"].Value;
+                var assemblyName = match.Groups["assemblyName"].Value;
+                Assembly assembly = null;
+                if (!String.IsNullOrEmpty(assemblyName))
+                {
+                    assembly = System.Reflection.Assembly.Load(new AssemblyName(assemblyName));
+                }
+                else
+                {
+                    if (serviceProvider == null)
+                    {
+                        throw new ArgumentNullException("serviceProvider");
+                    }
+                    IRootObjectProvider rootObjectProvider = serviceProvider.GetService<IRootObjectProvider>();
+                    if (rootObjectProvider == null)
+                    {
+                        throw new ArgumentException("serviceProvider did not provide an IRootObjectProvider");
+                    }
+
+                    // Find the root visual element (our view)
+                    var visualElement = rootObjectProvider.RootObject as VisualElement;
+                    var visualElementType = visualElement.GetType();
+                    assembly = visualElementType.GetTypeInfo().Assembly;
+                }
+                
+#if DEBUG
+                if (assembly != null && !assembly.GetManifestResourceNames().Contains(resourceName))
+                {
+                    System.Diagnostics.Debug.WriteLine("ImageResourceExtension::ProvideValue - No resource found for stream name '{0}'.", Source);
+                }
+                else
+                {
+                    System.Diagnostics.Debug.WriteLine("ImageResourceExtension::ProvideValue - Found resource '{0}'.", Source);
+                }
+#endif
+                // Do your translation lookup here, using whatever method you require
+                imageSource = ImageSource.FromStream(() => assembly.GetManifestResourceStream(resourceName));
+            }
+            else
+            {
+                System.Diagnostics.Debug.WriteLine("ImageResourceExtension::ProvideValue - Unable to parse source string '{0}'.", Source);
+            }
 
             return imageSource;
         }
